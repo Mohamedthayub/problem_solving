3174f10dec0bb715981cf476251dcabb2b684c63
buyath <buyaththayub@gmail.com>
2025-03-04 00:48:38 +0000

    two test file added

---
+++ test.java
@@ -0,0 +1,10 @@
+public class main{
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        String temp = "";
+        for(int i = 0; i<arr.length; i++){
+            temp = arr[i] + " " + temp ;
+        }
+        System.out.print(temp);
+    }
+}
---
+++ test.js
@@ -0,0 +1,7 @@
+
+
+var sum  = 1;
+for(var i = 0; i<=n; i++){
+    sum = sum * i;
+}
+console.log(sum);
