520355e3fd3d6e60c95a65315318a16abb8a9865
buyath <buyaththayub@gmail.com>
2025-03-16 23:53:29 +0000

    one problem done in array move all zero to end

--- test.java
+++ test.java
@@ -2,9 +2,8 @@ import java.util.HashSet;
 import java.util.Set;
 public class main{
     public static void main(String[] args){
-        int[] arr = {1,2,3,4,5,};
-        int[] arr2 = {1,2,3};
-        arr.add(arr2[0]);
-        System.out.print(arr);
+        int[] arr = {1,2,3,4,5};
+        arr.add(6);
+        System.out.print(arrays.toString(arr));
     } 
 }
