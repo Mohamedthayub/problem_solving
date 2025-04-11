d7bd819a291c4c8b068e0aeed3d81c5574731a34
buyath <buyaththayub@gmail.com>
2025-04-11 18:07:36 +0000

    i have learnt hashmap functions

---
+++ hashmap/hashmap_functions/test.js
@@ -0,0 +1,19 @@
+let map = new Map();// this is how create map
+console.log(map); // here we print the map
+
+map.set("apple",10);
+map.set("banana",20);
+map.set("jackfruit",40); // we set the value in map
+console.log(map);
+
+
+console.log(map.has("apple")); // we check if map has the "banana" key
+
+
+
+console.log(map.get("banana")); // we get value with banana key
+
+map.delete("banana"); // we delete the value with the key
+console.log(map);
+map.clear(); // we clear the all value in map 
+console.log(map);
--- test.js
+++ test.js
@@ -1,11 +1,14 @@
-let arr = [5,4,3,2,1];
-for(let i = 0; i<arr.length; i++){
-    for(let j = 0; j<arr.length - i - 1 ; j++){
-        if(arr[j] > arr[j+1]){
-            let temp = arr[j+1];
-            arr[j + 1] = arr[j];
-            arr[j] = temp;
+function check(arr){
+    let first = 0;
+    let last = arr.length - 1;
+    while(first < last){
+        if(arr[first] != arr[last]){
+            return false;
         }
+        first++;
+        last--;
     }
+    return true;
+    
 }
-console.log(arr);
+console.log(check([1,2,3,4,5]));
