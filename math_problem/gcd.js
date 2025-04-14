3e9f981d60df6e88205464624314c361ea506ffa
buyath <buyaththayub@gmail.com>
2025-04-14 19:29:41 +0000

    gcd another approach added

--- math_problem/gcd.js
+++ math_problem/gcd.js
@@ -9,3 +9,16 @@ function  findGcd(num1,num2){
     return Math.max(...gcd);
 }
 console.log(findGcd(12,12));
+
+// this logic is first came in my mind  i have an another approach 
+
+function findGcd(num1,num2){
+    let gcd = 1;
+    for(let i = 1; i<=num2; i++){
+        if(num1 % i == 0 && num2 % i == 0){
+            gcd = i;
+        }
+    }
+    return gcd;
+}
+
