9b47057954ab3c6a524ed698c6dd1d234a29f9a2
buyath <buyaththayub@gmail.com>
2025-05-08 23:08:19 +0000

    four pattern done

---
+++ pattern_printing_revise/pattern3.java
@@ -0,0 +1,21 @@
+public class Main{
+    public static void main (String[] main){
+        int col = 5; 
+        for(int i = 0; i<col; i++){
+            for(int j = 1; j<=col - i; j++){
+                System.out.print("* ");
+            }
+            for(int j = 1; j<=i; j++){
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
+/*
+* * * * * 
+* * * *  
+* * *   
+* *    
+*     
+*/
---
+++ pattern_printing_revise/pattern4.java
@@ -0,0 +1,23 @@
+public class main{
+    public static void main(String[] args){
+        int col = 5;
+        for(int i = 0; i<col; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j<=col - i; j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
+
+/*
+*****
+ ****
+  ***
+   **
+    *
+
+*/
