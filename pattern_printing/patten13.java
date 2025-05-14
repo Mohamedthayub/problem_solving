06ab9ef1da2fb7f8f6af8f79032985ca96cd6dcd
buyath <buyaththayub@gmail.com>
2025-05-14 23:24:00 +0000

    patter13 done

--- pattern_printing/patten13.java
+++ pattern_printing/patten13.java
@@ -15,12 +15,31 @@ public class Main{
             space += 2;
             System.out.println();
         }
-     
+        int col2 = 4;
+         for(int i = 1; i<=col2; i++){
+            int space2 = 0;
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int k = 1; k<= 2 *(col2 - i); k++){
+                System.out.print(" ");
+            }
+            for(int t = 1; t<=i; t++){
+                System.out.print("*");
+            }
+            System.out.println();
+            space2 += 2;
+        }
     }
 }
+
 /*
 ********
 ***  ***
 **    **
 *      *
+*      *
+**    **
+***  ***
+********
 */
--- pattern_printing/test.java
+++ pattern_printing/test.java
@@ -1,21 +1,8 @@
 public class Main{
     public static void main(String[] args){
         int col = 5;
-        // int space = 0;
-        int count = 15;
-        for(int i = 1; i<=col; i++){
-            for(int  j = 1; j<=i; j++){
-                 System.out.print(count + " ");
-                 count-= 1;
-            }
-            System.out.println();
+        
            
-        }
     }
 }
-/*
-********
-***  ***
-**    **
-*      *
-*/
+
