eac463c260630e58b46e25e6b61caa8c220a374e
buyath <buyaththayub@gmail.com>
2025-06-10 23:48:24 +0000

    isPair problem done by two pointer approach

---
+++ two_pointer/isPair.js
@@ -0,0 +1,18 @@
+function isPair(arr,x){
+    let i = 0;
+    let j = arr.length - 1;
+    while(i < j){
+        if(arr[i] + arr[j] == x){
+            return true;
+        }
+        else if (arr[i] + arr[j] > x){
+            j -= 1;
+        }
+        else {
+            i +=1;
+        }
+    }
+    return false;
+}
+
+console.log(isPair([2,5,8,12,30],17));
--- two_pointer/test.js
+++ two_pointer/test.js
@@ -1,11 +1,11 @@
-function findTheTarget(arr,target){
+function targetSum(arr,x){
     for(let i = 0; i<arr.length; i++){
         for(let j = i+1; j<arr.length; j++){
-            if(arr[i] + arr[j] == target){
+            if(arr[i] + arr[j] == x){
                 return true;
             }
         }
     }
-    return  false;
+    return false;
 }
-console.log(findTheTarget([1,2,3,4,5],10));
+console.log(targetSum([1,23,3,4,5],5));
