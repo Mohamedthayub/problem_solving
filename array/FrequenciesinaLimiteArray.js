63a156f6f6119a526f2b2ad8da72da644f1a2a2d
buyath <buyaththayub@gmail.com>
2025-06-13 22:20:49 +0000

    optimize code added in frequencieslimitedarray problem

--- array/FrequenciesinaLimiteArray.js
+++ array/FrequenciesinaLimiteArray.js
@@ -17,6 +17,19 @@ class Solution {
         return counts;
     }
 }
+
+let arr = [2, 3, 2, 3, 5];
+let n = arr.length;
+let result = new Array(n).fill(0);
+
+for (let i = 0; i < n; i++) {
+    let num = arr[i];
+    if (num >= 1 && num <= n) {
+        result[num - 1]++;
+    }
+}
+
+return result;
 /*
 You are given an array arr[] containing positive integers. The elements in the array arr[] range from 1 to n (where n is the size of the array), and some numbers may be repeated or absent. Your task is to count the frequency of all numbers in the range 1 to n and return an array of size n such that result[i] represents the frequency of the number i (1-based indexing).
 
