0545de688acb6627f54700c03dc08cc54326606a
buyath <buyaththayub@gmail.com>
2025-06-27 23:23:33 +0000

    Initial solution for stock profit problem (brute-force, not optimized)

--- recursion/recursion_basic_revise/factorial.js
+++ recursion/recursion_basic_revise/factorial.js
@@ -6,3 +6,11 @@ function factorial(num,fact){
     factorial(num- 1, fact * num);
 }
 factorial(5,1);
+
+function factorial2(num2){
+    if(num2 == 1){
+        return 1;
+    }
+    return num2 * factorial2(num2 - 1);
+}
+console.log(factorial2(5));
