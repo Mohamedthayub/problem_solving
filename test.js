eeaa6c2dcb351b7bc651d840a9dbed5abfa0e252
buyath <buyaththayub@gmail.com>
2025-07-09 23:16:28 +0000

    this is for test

--- test.js
+++
@@ -1,8 +0,0 @@
-// output = [10,30,40,45,60];
-let arr = [10,20,10,5,15];
-let prefixSum = [];
-prefixSum[0] = arr[0];
-for(let i =  1; i<arr.length; i++){
-    prefixSum[i] = prefixSum[i-1] + arr[i]; 
-}
-console.log(prefixSum);
