8eeb532c00bb5331dd1edb3a0ea453783a602fa1
buyath <buyaththayub@gmail.com>
2025-07-19 06:41:25 +0000

    findAllPairsGivenSum problem done with brute force approach

--- chatgpt_easy_problem/findAllPairsGivenSum.js
+++ chatgpt_easy_problem/findAllPairsGivenSum.js
@@ -2,11 +2,11 @@ function findThePair(arr,target){
     let pairs = [];
     for(let i = 0; i<arr.length-1; i++){
         for(let j = i+1; j<arr.length; j++){
-            if(arr[i] + arr[j] == target){
+            if(arr[i] + arr[i] == target){
                 pairs.push([arr[i],arr[j]]);
             }
         }
     }
-    return (pairs.length != 0) ? pairs : -1;
+    return (pairs.length != 0) ? pairs : [];
 }
 console.log(findThePair([1, 5, 7, -1, 5],6));
