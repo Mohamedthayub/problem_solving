087d29beab697b66253c7f6c8db7dbb6338aa3b7
buyath <buyaththayub@gmail.com>
2025-07-19 07:25:17 +0000

    Solved findAllPairGivenSum using two-pointer approach

--- chatgpt_easy_problem/findAllPairsGivenSum.js
+++ chatgpt_easy_problem/findAllPairsGivenSum.js
@@ -10,3 +10,27 @@ function findThePair(arr,target){
     return (pairs.length != 0) ? pairs : [];
 }
 console.log(findThePair([1, 5, 7, -1, 5],6));
+
+function findPairs(arr, target) {
+    let pairs = [];
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+
+        if (sum === target) {
+            pairs.push([arr[left], arr[right]]);
+            left++;
+            right--;
+        } else if (sum > target) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+    return pairs.length > 0 ? pairs : [];
+}
+
+console.log(findPairs([1, 3, 4, 6, 8, 9], 7)); 
+//  Output: [ [ 1, 6 ], [ 3, 4 ] ]
