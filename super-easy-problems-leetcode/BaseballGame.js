02005078c9ff163f2870ba17fbd761ddcee1fa9e
buyath <buyaththayub@gmail.com>
2025-08-22 00:47:15 +0000

    baseball game problem solved in leetcode super easy problem

---
+++ super-easy-problems-leetcode/BaseballGame.js
@@ -0,0 +1,71 @@
+
+var calPoints = function(operations) {
+    let temp = [];
+operations.forEach((item) => {
+    if(item == Number(item)){
+        temp.push(Number(item));
+    }
+    if(item == "C"){
+        temp.pop();
+    }
+    if(item == "D"){
+        let double = 2 * temp[temp.length - 1];
+        temp.push(double);      
+    }
+    if(item == "+"){
+        let last = temp[temp.length - 1];
+        let last2 = temp[temp.length - 2];
+        temp.push(last + last2);
+    }
+    
+})
+let result = temp.reduce((acc,sum) => acc  + sum,0);
+return result;
+};
+/*
+You are keeping the scores for a baseball game with strange rules. At the beginning of the game, you start with an empty record.
+
+You are given a list of strings operations, where operations[i] is the ith operation you must apply to the record and is one of the following:
+
+An integer x.
+Record a new score of x.
+'+'.
+Record a new score that is the sum of the previous two scores.
+'D'.
+Record a new score that is the double of the previous score.
+'C'.
+Invalidate the previous score, removing it from the record.
+Return the sum of all the scores on the record after applying all the operations.
+
+The test cases are generated such that the answer and all intermediate calculations fit in a 32-bit integer and that all operations are valid.
+
+ 
+
+Example 1:
+
+Input: ops = ["5","2","C","D","+"]
+Output: 30
+Explanation:
+"5" - Add 5 to the record, record is now [5].
+"2" - Add 2 to the record, record is now [5, 2].
+"C" - Invalidate and remove the previous score, record is now [5].
+"D" - Add 2 * 5 = 10 to the record, record is now [5, 10].
+"+" - Add 5 + 10 = 15 to the record, record is now [5, 10, 15].
+The total sum is 5 + 10 + 15 = 30.
+Example 2:
+
+Input: ops = ["5","-2","4","C","D","9","+","+"]
+Output: 27
+Explanation:
+"5" - Add 5 to the record, record is now [5].
+"-2" - Add -2 to the record, record is now [5, -2].
+"4" - Add 4 to the record, record is now [5, -2, 4].
+"C" - Invalidate and remove the previous score, record is now [5, -2].
+"D" - Add 2 * -2 = -4 to the record, record is now [5, -2, -4].
+"9" - Add 9 to the record, record is now [5, -2, -4, 9].
+"+" - Add -4 + 9 = 5 to the record, record is now [5, -2, -4, 9, 5].
+"+" - Add 9 + 5 = 14 to the record, record is now [5, -2, -4, 9, 5, 14].
+The total sum is 5 + -2 + -4 + 9 + 5 + 14 = 27.
+
+
+*/
--- test.js
+++ test.js
@@ -1,7 +1,22 @@
-let  s = ["h","e","l","l","o"];
-let reverse = [];
-for(let i = 1; i<=s.length; i++){
-    reverse[i-1] = s[s.length - i];
-}
-
-console.log(reverse);
+let temp = [];
+let ops = ["5","2","C","D","+"];
+ops.forEach((item) => {
+    if(item == Number(item)){
+        temp.push(Number(item));
+    }
+    if(item == "C"){
+        temp.pop();
+    }
+    if(item == "D"){
+        let double = 2 * temp[temp.length - 1];
+        temp.push(double);      
+    }
+    if(item == "+"){
+        let last = temp[temp.length - 1];
+        let last2 = temp[temp.length - 2];
+        temp.push(last + last2);
+    }
+    
+})
+let result = temp.reduce((acc,sum) => acc  + sum);
+console.log(result);
