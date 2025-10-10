a4b9bd9e880bf8bd3e8c730bfb251388a8736ca2
buyath <buyaththayub@gmail.com>
2025-10-10 00:21:57 +0000

    solved check eqaul arrays problem

---
+++ curios-freak-coding-sheet/array/check-equal-arrays.js
@@ -0,0 +1,34 @@
+class Solution {
+    checkEqual(a, b) {
+        // code here
+        let sum1 = 0;
+        let sum2 = 0;
+        for(let i = 0; i<a.length; i++){
+            sum1 = sum1 + a[i];
+        }
+        for(let j = 0; j<b.length; j++){
+            sum2 = sum2  + b[j];
+        }
+        if(sum1 == sum2){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+
+/*
+Given two arrays a[] and b[] of equal size, the task is to find whether the elements in the arrays are equal.
+Two arrays are said to be equal if both contain the same set of elements, arrangements (or permutations) of elements may be different though.
+Note: If there are repetitions, then counts of repeated elements must also be the same for two arrays to be equal.
+
+Examples:
+
+Input: a[] = [1, 2, 5, 4, 0], b[] = [2, 4, 5, 0, 1]
+Output: true
+Explanation: Both the array can be rearranged to [0,1,2,4,5]
+Input: a[] = [1, 2, 5], b[] = [2, 4, 15]
+Output: false
+Explanation: a[] and b[] have only one common value.
+*/
