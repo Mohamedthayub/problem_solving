a2bbe7125ea628f7920009aa46ab70dd7a1ba438
buyath <buyaththayub@gmail.com>
2025-10-13 22:08:15 +0000

    solved drink problem in zestober

--- zestober-problems/drink.js
+++ zestober-problems/drink.js
@@ -1,28 +1,3 @@
-class Solution {
-	countUniqueDrinkPairs(drinks_str) {
-		// TODO: implement the logic to count unique drinks and compute total unique pairs (nC2)
-		let result = drinks_str.split(" ");
-		let frequency = new Map();
-		for(let i =  0; i<result.length; i++){
-		    if(!frequency.has(result[i])){
-		        frequency.set(result[i] , 1);
-		    }
-		    else{
-		        frequency.set(result[i] , frequency.get(result[i]) + 1);
-		    }
-		}
-		let pairs = frequency.size * (frequency.size  - 1) / 2;
-		return pairs;
-	}
-}
-
-// Example usage
-const solution = new Solution();
-console.log(solution.countUniqueDrinkPairs("Apple Orange Mango Pineapple")); // Expected: 6
-console.log(solution.countUniqueDrinkPairs("Mango Apple Apple Orange")); // Expected: 3
-console.log(solution.countUniqueDrinkPairs("Mango")); // Expected: 0
-
-
 /*
 At a school fair, a juice stall is offering a variety of drinks like apple juice, orange juice, and lemonade. The drinks are listed by their names in a single string, separated by spaces. The stall owner wonders: “If I have n unique drinks, how many different pairs can I mix?” At first, he could try listing them one by one… but that feels slow. Instead, he starts to think: Each drink can pair with every other drink. So the first drink makes (n-1) pairs, the second makes (n-2) new pairs (since one was already counted), and so on. Adding them up gives a neat shortcut. 👉 Can you figure out how this reasoning leads to the formula for the number of pairs? (2n)=2n⋅(n−1). Your task is to help the stall owner by finding how many unique drinks are available, and then computing how many unique pairs can be made.
 Task
