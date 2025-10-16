ab7c71f0832f2535906c9cd944926bc734e10384
buyath <buyaththayub@gmail.com>
2025-10-16 19:12:52 +0000

    added problem statement to movezerotoend

--- curios-freak-coding-sheet/two-pointer/problems/moveZerotoEnd.js
+++ curios-freak-coding-sheet/two-pointer/problems/moveZerotoEnd.js
@@ -1,16 +1,35 @@
-function moveZeroToEnd(arr){
-    let index = 0;
-    for(let j = 0; j<arr.length; j++){
-        if(arr[j]  != 0){
-            arr[index] = arr[j];
-            index++;
+
+class Solution {
+    pushZerosToEnd(arr) {
+        // code here
+        let index = 0;
+        for(let i = 0; i<arr.length; i++){
+            if(arr[i] != 0){
+                arr[index] = arr[i];
+                index++;
+            }
         }
+        for(let j = index; j<arr.length; j++){
+            arr[j] = 0;
+        }
+        return arr;
+        
     }
-    for(let i = index; i<arr.length; i++){
-        arr[i] = 0;
-    }
-    return arr;
 }
-console.log(moveZeroToEnd([0,0,0,1,2,3]));
-console.log(moveZeroToEnd([1,0,2,0,3,0]));
-console.log(moveZeroToEnd([5,5,5,5,5]));
+
+/*
+You are given an array arr[] of non-negative integers. You have to move all the zeros in the array to the right end while maintaining the relative order of the non-zero elements. The operation must be performed in place, meaning you should not use extra space for another array.
+
+Examples:
+
+Input: arr[] = [1, 2, 0, 4, 3, 0, 5, 0]
+Output: [1, 2, 4, 3, 5, 0, 0, 0]
+Explanation: There are three 0s that are moved to the end.
+Input: arr[] = [10, 20, 30]
+Output: [10, 20, 30]
+Explanation: No change in array as there are no 0s.
+Input: arr[] = [0, 0]
+Output: [0, 0]
+Explanation: No change in array as there are all 0s.
+
+*/
