0854c820ebf02266884e12e0db111331566765ea
buyath <buyaththayub@gmail.com>
2025-12-18 23:24:53 +0000

    solved count odd even with hashmap

--- revise-curios-freak-coding-sheet/revise-array/test.js
+++ revise-curios-freak-coding-sheet/revise-array/test.js
@@ -1,17 +1,12 @@
-function checkArr(arr1,arr2){
-    let sum1 = 0; 
-    let sum2 = 0;
-    for(let i = 0; i<arr1.length; i++){
-        sum1 = sum1 + arr1[i];
-    }
-    for(let j = 0; j<arr2.length; j++){
-        sum2 = sum2 + arr2[j]; 
-    }
-    if(sum1 == sum2){
-        return true;
-    }
-    else{
-        return false;
-    }
+let number = [1,2,3,4,5,6];
+let map = new Map();
+
+for (let value of number) {
+    let key = value % 2 === 0 ? "even" : "odd";
+    map.set(key, (map.get(key) || 0) + 1);
 }
-console.log(checkArr( [1, 2, 5], [2, 4, 15]))
+
+console.log(map);
+
+
+// count the odd and even number in the array 
